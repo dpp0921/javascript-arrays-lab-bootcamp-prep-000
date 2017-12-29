cb45f071dee1cafa8e312d6cd85f2110c82968ab
Chen Ye <dpp0921@gmail.com>
2017-12-29 17:01:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const app = "I don't do much."
+//var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+function appendKitten(name){
+  var newArray = [...kittens, name]
+  // or ES6 way
+  // var newArray = [...kittens];
+  //newArray.push(name)
+  return newArray
+}
+function prependKitten(name){
+  var newArray = [name, ...kittens]
+  return newArray;
+}
+function removeLastKitten(){
+  var newArray = kittens.pop()
+  return newArray;
+}
