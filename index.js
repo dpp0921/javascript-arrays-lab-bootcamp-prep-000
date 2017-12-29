8bb655f7f8841e1e7ab0ce1ccd31b4812bfcd2f4
Chen Ye <dpp0921@gmail.com>
2017-12-29 17:24:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ function removeLastKitten(){
   return newArray;
 }
 function removeFirstKitten(){
-  var newArray = kittens.splice(1)
+  var newArray = kittens.splice(kittens.length)
   return newArray
 }
